34221152bb0e27fb2db0d281d9b4eff95842d371
Bohdan Belei <bohdan.belei@outlook.com>
2023-11-04 01:23:07 +0200

    Update NabBar component with CustomLink.

--- reactportfolio/src/components/NavBar.js
+++ reactportfolio/src/components/NavBar.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import Link from 'next/link';
+import Logo from './Logo';
+import { CustomLink } from './CustomLink';
 
 const NavBar = () => {
   return (
     <header className='w-full px-32 py-8 font-medium flex items-center justify-between'>
       <nav>
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/projects">Projects</Link>
+        <CustomLink href="/" title="Home" className='mr-4' />
+        <CustomLink href="/about" title="About" className='mx-4' />
+        <CustomLink href="/projects" title="Projects" className='ml-4' />
       </nav>
-      <h2>Logo</h2>
       <nav>
         <Link href="/" target={"_blank"}>LI</Link>
         <Link href="/" target={"_blank"}>LI</Link>
         <Link href="/" target={"_blank"}>LI</Link>
         <Link href="/" target={"_blank"}>LI</Link>
       </nav>
+      <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
+        <Logo />
+      </div>
     </header>
   );
 }

2c32582c4757ddc8f19d39a501111b59451fe3ae
Bohdan Belei <bohdan.belei@outlook.com>
2023-11-04 16:44:43 +0200

    Add header social media icons.

--- reactportfolio/src/components/NavBar.js
+++ reactportfolio/src/components/NavBar.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import Link from 'next/link';
 import Logo from './Logo';
 import { CustomLink } from './CustomLink';
+import { ExternalLink, LinkedInIcon, PinterestIcon } from './Icons';
+import { motion } from "framer-motion"
 
 const NavBar = () => {
   return (
@@ -11,11 +12,32 @@ const NavBar = () => {
         <CustomLink href="/about" title="About" className='mx-4' />
         <CustomLink href="/projects" title="Projects" className='ml-4' />
       </nav>
-      <nav>
-        <Link href="/" target={"_blank"}>LI</Link>
-        <Link href="/" target={"_blank"}>LI</Link>
-        <Link href="/" target={"_blank"}>LI</Link>
-        <Link href="/" target={"_blank"}>LI</Link>
+      <nav className='flex items-center justify-center flex-wrap text-3xl'>
+        <motion.a
+          href="https://www.linkedin.com" target={"_blank"}
+          whileHover={{ y: -3 }}
+          whileTap={{ scale: 0.9 }}
+          className='w-6 mx-2'>
+          <LinkedInIcon />
+        </motion.a>
+        <motion.a href="https://pinterest.com" target={"_blank"}
+          whileHover={{ y: -3 }}
+          whileTap={{ scale: 0.9 }}
+          className='w-6 mx-2'>
+          <PinterestIcon />
+        </motion.a>
+        <motion.a href="/" target={"_blank"}
+          whileHover={{ y: -3 }}
+          whileTap={{ scale: 0.9 }}
+          className='w-6 mx-2'>
+          <ExternalLink />
+        </motion.a>
+        <motion.a href="/" target={"_blank"}
+          whileHover={{ y: -3 }}
+          whileTap={{ scale: 0.9 }}
+          className='w-6 ml-2'>
+          <ExternalLink />
+        </motion.a>
       </nav>
       <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
         <Logo />

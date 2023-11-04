7fb64b8021b099cd4e1ec7c006b261b58d25e64c
Bohdan Belei <bohdan.belei@outlook.com>
2023-11-04 15:33:04 +0200

    Enhance logo animation.

--- reactportfolio/src/components/Logo.js
+++ reactportfolio/src/components/Logo.js
@@ -7,15 +7,21 @@ const MotionLink = motion(Link)
 const Logo = () => {
   return (
     <div className='flex items-center justify-center mt-2'>
-        <MotionLink 
-            className='w-16 h-16 bg-dark text-light text=2xl font-bold flex items-center justify-center rounded-full'
-            whileHover={{
-                scale:1.5,
-                backgroundColor:["#121212", "rgba(131,58,180,1)","rgba(253,29,29,1)","rgba(252,176,69,1)","rgba(131,58,180,1)", "#121212"],
-                transition:{duration:1, repeat:Infinity}
-            }}
-            href="/">NB
-        </MotionLink>
+      <MotionLink
+        className='w-16 h-16 bg-dark text-light text-2xl font-bold flex items-center justify-center rounded-full'
+        whileHover={{
+          scale: [1, 1.2, 1],
+          backgroundColor: [
+            "rgb(192, 0, 0)",
+            "rgb(255, 140, 0)",
+            "rgb(192, 0, 0)"
+          ],
+          transition: { duration: 1, ease: "easeInOut", repeat: Infinity }
+        }}
+        initial={{ backgroundColor: "rgb(0, 0, 0)" }}
+        exit={{ backgroundColor: "rgb(0, 0, 0)" }}
+        href="/">NB
+      </MotionLink>
     </div>
   )
 }

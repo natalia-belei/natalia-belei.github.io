2e121d8cfeefa262a759dff86bfdfc0862db570a
Bohdan Belei <bohdan.belei@outlook.com>
2023-11-05 14:29:21 +0200

    Fix formatting.

--- reactportfolio/src/components/LiIcon.js
+++ reactportfolio/src/components/LiIcon.js
@@ -11,7 +11,9 @@ const LiIcon = ({ reference }) => {
         <figure className='absolute left-0 stroke-dark'>
             <svg className='-rotate-90' width="75" height="75" viewBox='0 0 100 100'>
                 <circle cx="75" cy="50" r="20" className='stroke-primary stroke-1 fill-none' />
-                <motion.circle cx="75" cy="50" r="20" className='stroke-[5px] fill-light'
+                <motion.circle
+                    cx="75" cy="50" r="20"
+                    className='stroke-[5px] fill-light'
                     style={{
                         pathLength: scrollYProgress
                     }}

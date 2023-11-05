d2ee4fac34af53a23c85d8301fd7e111a1dd2669
Bohdan Belei <bohdan.belei@outlook.com>
2023-11-05 14:37:40 +0200

    Fix basePath configuration.

--- reactportfolio/next.config.js
+++ reactportfolio/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-  basePath: '/test-react-portfolio',
+  basePath: '/jobs-react-portfolio',
   reactStrictMode: true,
   images: {
     unoptimized: true,

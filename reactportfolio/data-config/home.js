91d865678057fdff598824fb3982fb492f415fa2
Natalia Belei <80248788+natalia-belei@users.noreply.github.com>
2023-11-08 16:51:41 +0200

    Update home.js

--- reactportfolio/data-config/home.js
+++ reactportfolio/data-config/home.js
@@ -1,7 +1,7 @@
 import { contacts as contactsConfig } from "./contacts.js";
 
 export const home = {
-    mainTitle: "Transforming Vision Into Reality Through Design",
+    mainTitle: "Bringing Vision to Life Through Design",
     meta: {
         title: `${contactsConfig.ownerFullName} | Portfolio`,
         description: "UI/UX Designer Portfolio.",

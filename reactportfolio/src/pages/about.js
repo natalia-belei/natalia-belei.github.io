d2c24b6b4000f5ac2f4a4b89947b7ed9c1775c49
Bohdan Belei <bohdan.belei@outlook.com>
2023-11-10 13:21:03 +0200

    Enhance profile picture shadow

--- reactportfolio/src/pages/about.js
+++ reactportfolio/src/pages/about.js
@@ -37,8 +37,8 @@ const about = () => {
                             <h2 className='mb-4 text-lg font-bold uppercase text-dark/75'>Summary</h2>
                             {renderSummary()}
                         </div>
-                        <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8'>
-                            <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-2xl bg-dark' />
+                        <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8'
+                            style={{ boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.2), 0px 6px 20px rgba(0, 0, 0, 0.15), 0px 8px 30px rgba(0, 0, 0, 0.1)' }}>
                             <Image src={profilePic} alt={aboutPageConfig.meta.profilePicAlt} className='w-full h-auto rounded-2xl' />
                         </div>
                         <div className='col-span-2 flex flex-col items-end justify-between'>

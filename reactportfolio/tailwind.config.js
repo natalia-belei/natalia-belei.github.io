ede6e4a00980498322e27c83189d551e433f5787
Bohdan Belei <bohdan.belei@outlook.com>
2023-11-10 19:32:44 +0200

    Add responsive breakpoints to Tailwind CSS config

--- reactportfolio/tailwind.config.js
+++ reactportfolio/tailwind.config.js
@@ -21,6 +21,25 @@ module.exports = {
         primaryDark: "#58E6D9", // 80,230,217
       },
     },
+    screens: {
+      "2xl": { max: "1535px" },
+      // => @media (max-width: 1535px) { ... }
+
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: "767px" },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: "639px" },
+      // => @media (max-width: 639px) { ... }
+
+      xs: { max: "479px" },
+      // => @media (max-width: 479px) { ... }
+    },
     animation: {
       'spin-slow': 'spin 6s linear infinite',
       'pulse': 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
@@ -32,4 +51,3 @@ module.exports = {
 
   plugins: [],
 }
-

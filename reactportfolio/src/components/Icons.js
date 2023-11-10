ebf5511756fac19ff3a1bdba221660e804aa5d50
Bohdan Belei <bohdan.belei@outlook.com>
2023-11-10 17:53:24 +0200

    Fix prop mismatch warning in BehanceIcon

--- reactportfolio/src/components/Icons.js
+++ reactportfolio/src/components/Icons.js
@@ -38,7 +38,7 @@ export const PinterestIcon = ({ className, ...rest }) => (
     </svg>
 );
 
-export const BehanceIcon = (className, ...rest) => (
+export const BehanceIcon = ({ className, ...rest }) => (
     <svg
         xmlns="http://www.w3.org/2000/svg"
         width="1em"

ff8d2d0240c58e371fdc112eb64db04381a1972c
Bohdan Belei <bohdan.belei@outlook.com>
2023-11-10 20:46:11 +0200

    Add PropTypes validation to CustomLink component

--- reactportfolio/src/components/CustomLink.js
+++ reactportfolio/src/components/CustomLink.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import PropTypes from 'prop-types';
 
 export const CustomLink = ({ href, title, className = "" }) => {
     const router = useRouter();
@@ -11,10 +12,16 @@ export const CustomLink = ({ href, title, className = "" }) => {
             <span className={`
                 h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5
                 group-hover:w-full transition-[width] ease duration-300
-                ${ router.asPath === href ? 'w-full' : 'w-0' }
+                ${router.asPath === href ? 'w-full' : 'w-0'}
             `}>
                 &nbsp;
             </span>
         </Link>
     );
+};
+
+CustomLink.propTypes = {
+    href: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    className: PropTypes.string,
 };

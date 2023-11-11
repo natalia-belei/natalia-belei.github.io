188c58b90b05c988451c447f2a1d1856dd8972c8
Natalia Belei <80248788+natalia-belei@users.noreply.github.com>
2023-11-11 22:53:10 +0200

    Update home.js

--- reactportfolio/data-config/home.js
+++ reactportfolio/data-config/home.js
@@ -8,5 +8,5 @@ export const homePageConfig = {
     },
 
     mainTitle: "Transforming Vision Into Reality Through Design",
-    summary: "As an accomplished UI/UX designer, I am committed to transforming concepts into captivating web experiences. Dive into my recent projects and articles, highlighting my proficiency in creating engaging user interfaces and user experiences.",
+    summary: "With a design journey spanning three impactful years, I've navigated the dynamic realms of both agency and freelance work. From collaborating within a creative agency to carving my path as a freelancer, my passion for UI/UX design has been the guiding force. This journey is complemented by a foundation in graphic design, weaving a narrative of creativity and adaptability. Join me on this visual expedition, where every project is a testament to experience, innovation, and a love for crafting meaningful digital interactions.",
 }

01e686876d4e3533aefe83a7ca5cbb14dcaaf4b2
Bohdan Belei <bohdan.belei@outlook.com>
2023-11-11 16:07:13 +0200

    Enhance responsiveness of the footer layout

--- reactportfolio/src/components/Footer.js
+++ reactportfolio/src/components/Footer.js
@@ -1,10 +1,12 @@
-import React from 'react'
-import Layout from './Layout'
+import React from 'react';
+import Layout from './Layout';
 
 const Footer = () => {
     return (
-        <footer className='w-full border-t-2 border-solid border-dark font-medium text-lg'>
-            <Layout className='py-8 items-center justify-between text-center'>
+        <footer className='w-full border-t-2 border-solid border-dark font-medium text-lg
+            sm:text-base'>
+            <Layout className='py-8 items-center justify-between text-center
+                lg:flex-col lg:py-6'>
                 <span className='text-medium'>
                     {new Date().getFullYear()} &copy; All Rights Reserved.
                 </span>

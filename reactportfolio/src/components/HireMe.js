37ad364ab10f760ea3404c130a07923453c859bb
Bohdan Belei <bohdan.belei@outlook.com>
2023-11-19 16:19:40 +0200

    Centralize HireMe button configuration

--- reactportfolio/src/components/HireMe.js
+++ reactportfolio/src/components/HireMe.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { CircularText } from './Icons';
 import Link from 'next/link';
 import { contactsConfig } from '/data-config/contacts.js';
+import { homePageConfig } from '../../data-config/home';
 
 const HireMe = () => {
     return (
@@ -13,13 +14,13 @@ const HireMe = () => {
                 <CircularText className="fill-dark animate-spin-slow" />
                 <Link
                     href={contactsConfig.ownerEmail}
-                    className='flex items-center justify-center
+                    className={`flex items-center justify-center text-center
                     absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2
                     bg-dark text-light shadow-md border border-solid border-dark
                     w-20 h-20 rounded-full font-semibold
                     hover:bg-light hover:text-dark
-                    md:w-12 md:h-12 md:text-[10px]'>
-                    Hire Me
+                    md:w-12 md:h-12 md:text-[10px]`}>
+                    {homePageConfig.hireMeButton.title && homePageConfig.hireMeButton.title}
                 </Link>
             </div>
         </div>

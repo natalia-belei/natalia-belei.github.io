73572d43a59471b591927f6f62166810feafbced
Bohdan Belei <bohdan.belei@outlook.com>
2023-11-19 16:35:57 +0200

    Change HireMe button content

--- reactportfolio/data-config/home.js
+++ reactportfolio/data-config/home.js
@@ -10,6 +10,6 @@ export const homePageConfig = {
     mainTitle: "Where creativity meets technology.",
     summary: "In a world driven by user-centric design, I specialize in transforming visions into seamless digital realities. Explore a portfolio where every pixel tells a story, and every interaction invites engagement.",
     hireMeButton: {
-        title: "Hire Me",
+        title: "Let's work together!",
     }
 }
--- reactportfolio/src/components/HireMe.js
+++ reactportfolio/src/components/HireMe.js
@@ -7,19 +7,20 @@ import { homePageConfig } from '../../data-config/home';
 const HireMe = () => {
     return (
         <div className='flex items-center justify-center overflow-hidden z-20 fixed left-4 bottom-4
-            md:absolute md:right-5 md:left-auto md:top-[-5px] md:bottom-auto
+            md:absolute md:right-5 md:left-auto md:top-[2px] md:bottom-auto
             sm:right-0'>
             <div className='w-48 h-auto flex items-center justify-center relative
                 md:w-24'>
-                <CircularText className="fill-dark animate-spin-slow" />
+                <CircularText className="fill-dark animate-spin-slow w-[130px]
+                md:w-[80px]" />
                 <Link
                     href={contactsConfig.ownerEmail}
-                    className={`flex items-center justify-center text-center
+                    className={`flex items-center justify-center text-center text-xs
                     absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2
                     bg-dark text-light shadow-md border border-solid border-dark
                     w-20 h-20 rounded-full font-semibold
                     hover:bg-light hover:text-dark
-                    md:w-12 md:h-12 md:text-[10px]`}>
+                    md:w-14 md:h-14 md:text-[8px] md:leading-3`}>
                     {homePageConfig.hireMeButton.title && homePageConfig.hireMeButton.title}
                 </Link>
             </div>

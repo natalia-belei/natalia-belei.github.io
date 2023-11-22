8ee56f5d315e665858d2541f08a149c1a1da1c48
Natalia Belei <80248788+natalia-belei@users.noreply.github.com>
2023-11-22 23:21:47 +0200

    Update projects.js

--- reactportfolio/data-config/projects.js
+++ reactportfolio/data-config/projects.js
@@ -113,7 +113,6 @@ export const projectsData = [
             {
                 type: CONTENT_TYPE.paragraph,
                 content: "The pet food delivery service app is designed to provide a convenient and hassle-free solution for pet owners to meet their pets nutritional needs. The target users of the app are tech-savvy pet owners, primarily residing in urban areas, who prioritize their pets' health, seek cost-effective options, and embrace the convenience of mobile apps for their pet-related needs.",
-",
                 color: "primary", // set to null or refer to the comment above for details
                 alignment: null // ("left", "center", "right", "justify") or null for default
             },

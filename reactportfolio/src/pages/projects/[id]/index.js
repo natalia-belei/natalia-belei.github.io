c4fa5390e35ab409faacdf2e7adbdee3603ff894
Bohdan Belei <bohdan.belei@outlook.com>
2023-11-24 19:05:36 +0200

    Adjust container width for project detail page

--- reactportfolio/src/pages/projects/[id]/index.js
+++ reactportfolio/src/pages/projects/[id]/index.js
@@ -19,34 +19,37 @@ const Index = ({ project }) => {
                 <TransitionEffect />
                 <main className='w-full flex items-start justify-center'>
                     <Layout className="pt-16 sm:pt-8">
-                        <article className='w-full flex flex-col items-center justify-center mb-24 
+                        <div className='container mx-auto
+                             max-w-5xl 2xl:max-w-3xl md:max-w-full md:mx-0'>
+                            <article className='w-full flex flex-col items-center justify-center mb-24 
                             lg:mb-16 md:mb-8 sm:mb-6 md:items-start'>
-                            <AnimatedText text={project.title} className='mb-8 md:mb-4 sm:mb-3' />
+                                <AnimatedText text={project.title} className='mb-8 md:mb-4 sm:mb-3' />
 
-                            <div className='flex flex-col justify-center overflow-hidden mb-4 w-full h-[65vh]
+                                <div className='flex flex-col justify-center overflow-hidden mb-4 w-full h-[65vh]
                                 xl:h-[60vh] lg:h-[55vh] md:h-[50vh] sm:h-[40vh] xs:h-[30vh]
                                 xl:mb-3 md:mb-2 xs:mb-1'>
-                                <ParallaxEffect offset={100}>
-                                    <div className='w-full h-full'>
-                                        <div className='relative w-full pt-[100%] 
+                                    <ParallaxEffect offset={100}>
+                                        <div className='w-full h-full'>
+                                            <div className='relative w-full pt-[100%] 
                                             lg:pt-[110%] md:pt-[120%] sm:pt-[130%] xs:pt-[135%]'>
-                                            <Image
-                                                src={project.image}
-                                                alt={project.title}
-                                                fill="responsive"
-                                                style={{ objectFit: 'cover' }}
-                                                priority={true}
-                                                sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw'
-                                            />
+                                                <Image
+                                                    src={project.image}
+                                                    alt={project.title}
+                                                    fill="responsive"
+                                                    style={{ objectFit: 'cover' }}
+                                                    priority={true}
+                                                    sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw'
+                                                />
+                                            </div>
                                         </div>
-                                    </div>
-                                </ParallaxEffect>
-                            </div>
+                                    </ParallaxEffect>
+                                </div>
 
-                            {project.detailsPage.map((item, index) => (
-                                <ContentRenderer key={index} data={item} />
-                            ))}
-                        </article>
+                                {project.detailsPage.map((item, index) => (
+                                    <ContentRenderer key={index} data={item} />
+                                ))}
+                            </article>
+                        </div>
                     </Layout>
                 </main>
             </>
